bc1a1b8051df9d22ce5a1fc2e9ecadfa706113f3
thunderthard <hars1302@gmail.com>
2022-11-09 13:33:31 -0500

    Cambio de relaciones cliente a pedido, factura a pedido y vendedor a factura

--- tecnobot/src/controllers/index.ts
+++ tecnobot/src/controllers/index.ts
@@ -5,3 +5,6 @@ export * from './producto-pedido.controller';
 export * from './cliente-pedido.controller';
 export * from './vendedor-factura.controller';
 export * from './factura-pedido.controller';
+export * from './pedido-cliente.controller';
+export * from './pedido-factura.controller';
+export * from './pedido-producto.controller';
